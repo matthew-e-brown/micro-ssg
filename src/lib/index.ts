c5d0e34236e0039a110fbaa1f84525e344acff6c
Matthew Brown <matthew.e.brown.17@gmail.com>
2022-05-04 18:19:56 -0400

    Bump version, add index barrel file

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+// Re-export helper options so users don't need to install Handlebars as a dep
+export { HelperOptions } from 'handlebars';
+
+export { compile } from './compiler';
+export { CompilerOptions } from './options';
