63d53acaf0401e36ea62e41ade9e302fca036f7a
Matthew Brown <matthew.e.brown.17@gmail.com>
2022-05-05 13:08:36 -0400

    Improve tests

---
+++ tests/contrived/data/third.md
@@ -0,0 +1,1 @@
+This should be rendered as HTML. Italics: *slanted!*
--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -8,59 +8,74 @@ import { compile } from '../src/lib/compiler';
 
 
 let tempDir: string;
-
 beforeAll(async () => tempDir = await mkdtemp(path.join(tmpdir(), 'test-')));
 afterAll(async () => await rm(tempDir, { recursive: true, force: true }));
 
 
-describe('Contrived, full test', () => {
+async function readFileToString(path: string) {
+    const buffer = await readFile(path);
+    return buffer.toString();
+}
 
-    const readToString = (path: string) => readFile(path).then(buff => buff.toString());
 
-    test('compile without crashing', async () => {
+async function compare(name: string, expectedLines: string[]) {
+    const pageContents = await readFileToString(path.join(tempDir, name));
+    const pageLines = pageContents.split(/\r?\n/g);
+    expect(pageLines).toStrictEqual(expectedLines);
+}
+
+
+describe('Compiler', () => {
+
+    it('should compile the input without crashing', async () => {
         await compile(path.join(__dirname, 'contrived'), {
-            dist: tempDir,
+            dest: tempDir,
             overwrite: true,
+            tsConfigPath: './tsconfig.json',
         });
     });
 
-    test('compare output with expected', async () => {
-        const indexContents = await readToString(path.join(tempDir, 'index.html'));
-        const secondContents = await readToString(path.join(tempDir, 'second.html'));
+    it('should compile index.html to the expected output', () => {
+        return compare('index.html', [
+            'Page: index',
+            '',
+            'Inserting partial:',
+            'head partial, pageTitle value = Index Page',
+            '',
+            'Inserting data:',
+            'key = value',
+            'hello = world',
+            '_shared.yeet = yolo',
+            '',
+            'Prepending hyphens:',
+            '-- world',
+            '',
+            'Surrounding with exclamation points:',
+            '!! why are we yelling !!',
+            '',
+        ]);
+    });
 
-        console.log(indexContents.split(/\r?\n/g));
-        console.log(secondContents.split(/\r?\n/g));
+    it('should compile second.html to the expected output', () => {
+        return compare('second.html', [
+            'Page: second',
+            '',
+            'Inserting partial:',
+            'head partial, pageTitle value = Second Page',
+            '',
+            'Inserting data:',
+            'title = Fish!',
+            '_shared.yeet = yolo',
+            '',
+        ]);
+    });
 
-        expect(indexContents.split(/\r?\n/g))
-            .toStrictEqual([
-                'Page: index',
-                '',
-                'Inserting partial:',
-                'head partial, pageTitle value = Index Page',
-                '',
-                'Inserting data:',
-                'key = value',
-                'hello = world',
-                '',
-                'Prepending hyphens:',
-                '-- world',
-                '',
-                'Surrounding with exclamation points:',
-                '!! why are we yelling !!',
-                ''
-            ]);
-
-        expect(secondContents.split(/\r?\n/g))
-            .toStrictEqual([
-                'Page: second',
-                '',
-                'Inserting partial:',
-                'head partial, pageTitle value = Second Page',
-                '',
-                'Inserting data:',
-                'title = Fish!',
-                ''
-            ]);
+    it('should compile third.html to the expected output', () => {
+        return compare('third.html', [
+            'Rendered markdown:',
+            '<p>This should be rendered as HTML. Italics: <em>slanted!</em></p>',
+            '',
+        ]);
     });
 
 });
