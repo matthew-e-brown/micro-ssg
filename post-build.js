39e10ea6311e88a18428a655f89cbc92c06340b2
Matthew Brown <matthew.e.brown.17@gmail.com>
2022-05-12 11:48:34 -0400

    Fix post-build.js ENOENT

--- post-build.js
+++ post-build.js
@@ -3,7 +3,7 @@ const { version } = require('./package.json');
 const { join } = require('path');
 
 // Read output file and replace fake variable with constant
-readFile(join(__dirname,'dist/bin/index.js'))
+readFile(join(__dirname,'dist/bin.js'))
     .then(buf => buf.toString())
     .then(str => str.replace('___VERSION_NUMBER___', `'${version}'`))
-    .then(str => writeFile('./dist/bin/index.js', str, 'utf-8'));
+    .then(str => writeFile('./dist/bin.js', str, 'utf-8'));
